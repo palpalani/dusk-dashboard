17557db497fcfbaf6fd0a23a7f578139a910f285
palPalani <palaniappan@innoppl.com>
2021-07-22 13:12:48 +0530

    Added tests

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BeyondCode\DuskDashboard\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use BeyondCode\DuskDashboard\DuskDashboardServiceProvider;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            DuskDashboardServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        include_once __DIR__.'/../database/migrations/create_dusk-dashboard_table.php.stub';
+        (new \CreatePackageTable())->up();
+        */
+    }
+}
